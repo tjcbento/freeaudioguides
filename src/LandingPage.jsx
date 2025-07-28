8bba1f96229bc63f4af4def4e9cf38cc8cb5065b
Tiago <tjc.bento@gmail.com>
2025-07-28 15:45:10 +0100

    Fixed styling

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { FaMapMarkedAlt, FaHeadphones } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
